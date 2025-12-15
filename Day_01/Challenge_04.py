231023891a451ae76a345eee33b018b4c0b3f78f
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2025-12-16 00:10:36 +0600

    Create Challenge_04.py

---
+++ Day_01/Challenge_04.py
@@ -0,0 +1,5 @@
+seconds = int(input("Enter the time in seconds: "))
+hours = seconds // 3600
+minutes = seconds % 3600  // 60
+remaining_seconds = seconds % 60
+print(f"{hours} hours and {minutes} minutes and {remaining_seconds} seconds ")
