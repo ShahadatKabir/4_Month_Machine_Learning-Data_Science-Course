f3f9588c1b3c8ef13767882eab8bd6fd4642d36b
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-01-13 00:52:38 +0600

    Update Problem_01.py

--- Day_05/Problem_01.py
+++ Day_05/Problem_01.py
@@ -1,1 +1,11 @@
 
+numbers = list(range(-500000, 500001))
+neg_number = -1
+for i in numbers:
+    if i == neg_number:
+        print(f"{neg_number} is present in the list.")
+
+num = set(range(-500000, 500001))
+for i in num:
+    if i == neg_number:
+        print(f"{neg_number} is present in the set.")
