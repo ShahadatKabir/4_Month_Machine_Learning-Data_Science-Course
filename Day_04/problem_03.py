eabea463f2d53f6a8cc882a9a678331b693fb060
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-01-13 00:34:14 +0600

    Create problem_03.py

---
+++ Day_04/problem_03.py
@@ -0,0 +1,8 @@
+data = []              
+data.append(1)          
+data.append(2)
+data.append(3)
+print(f"After adding all the numbers in a list named data\ndata = {data}") 
+
+data.pop()              
+print(f"After removing the last number from the list\ndata = {data}")  
