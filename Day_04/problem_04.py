b502e1c04cc213a25b4692d0d1f5bef1caa1211d
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-01-14 00:51:40 +0600

    Update problem_04.py

--- Day_04/problem_04.py
+++ Day_04/problem_04.py
@@ -1,7 +1,9 @@
+# The One-Line Architect
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print(f"Before Squaring the even numbers: {numbers}")
 
 numbers = [x**2 for x in numbers if x % 2 == 0]
 
-print(f"After Squaring the even numbers:{numbers}")   
+print(f"After Squaring the even numbers:{numbers}")    # Output: [4, 16, 36, 64, 100]
