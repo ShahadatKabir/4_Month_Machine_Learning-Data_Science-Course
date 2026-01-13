a3f3fde5ff6c303631350dc91f807992ddd82dd6
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-01-14 00:51:17 +0600

    Update problem_03.py

--- Day_04/problem_03.py
+++ Day_04/problem_03.py
@@ -1,8 +1,10 @@
-data = []              
-data.append(1)          
+# The Stack Emulator
+
+data = []               # an empty list
+data.append(1)          # .append() -> add new data /value at the end of previous data
 data.append(2)
 data.append(3)
-print(f"After adding all the numbers in a list named data\ndata = {data}") 
+print(f"After adding all the numbers in a list named data\ndata = {data}")  # Ouptu: [1, 2, 3]
 
-data.pop()              
-print(f"After removing the last number from the list\ndata = {data}")  
+data.pop()              # remove the element from the last
+print(f"After removing the last number from the list\ndata = {data}")        # Output: [1, 2]
