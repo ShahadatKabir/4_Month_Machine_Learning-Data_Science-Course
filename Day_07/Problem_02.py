ff25da54cc6d6a3449bd95fbc8042e9f07a36a68
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-01-14 00:22:52 +0600

    Update Problem_02.py

--- Day_07/Problem_02.py
+++ Day_07/Problem_02.py
@@ -1,1 +1,12 @@
 
+# The Math Safety Net 
+x = 0
+
+try:
+    result = 100 / x
+    print(f"Result: {result}")
+except ZeroDivisionError:
+    print("Any real number cannot divided by zero")
+
+else:
+    print("Result printed successfully.")
