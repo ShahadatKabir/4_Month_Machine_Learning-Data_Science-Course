e61b7eb4b976ad078bc7427f8371ef0ca403ff23
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-02-04 23:54:30 +0600

    Add file writing and reading functionality
    Create a script that writes to and reads from a file.

---
+++ Week_02/Day_13/Problem_1.py
@@ -0,0 +1,12 @@
+# The Safe Open
+
+with open("Anika.txt", "w") as f:
+    f.write("Hello World!\n")
+    
+
+
+
+
+with open("Anika.txt", "r") as f:
+    file = f.read()
+    print(file)                                  # Output:  Hello World!
