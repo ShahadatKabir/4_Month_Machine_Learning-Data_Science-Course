0fc52f7255565dc4fb7f8d07703ecd99c450dfff
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2026-03-04 10:22:14 +0600

    Add flatten_lists function to handle nested lists
    Implement a generator to flatten nested lists.

---
+++ Week_02/Day_14/problem_4.py
@@ -0,0 +1,9 @@
+def flatten_lists(nested_lists):
+    for item in nested_lists:
+        if isinstance(item, list):
+            yield from flatten_lists(item)
+        else:
+            yield item
+
+data_list = [1, [2, [3, 4]]]
+print(list(flatten_lists(data_list)))  
